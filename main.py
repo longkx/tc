6894d77359b3d8f51317009a042a35fc763a86cd
longkaixiang@cmict.chinamobile.com <Rx3hgGIt2oii>
2020-10-12 15:57:43 +0800

    modify

--- main.py
+++ main.py
@@ -11,19 +11,55 @@ def print_hi():
     return 'Hello world'
 
 
+def get_sum():
+    count = 0
+    with open("/tcdata/num_list.csv", "r") as f:
+        for line in f:
+            current = line.rstrip()
+            # print(current)
+            count = int(current) + count
+    return count
+
+
 # Press the green button in the gutter to run the script.
 # print(__name__)
 
+def get_max():
+    file_list = []
+    result_list = []
+    min_num = 0
+    with open("/tcdata/num_list.csv", "r") as f:
+        for line in f:
+            if line in file_list:
+                continue
+            else:
+                file_list.append(int(line))
+
+    length = 10
+    if int(len(file_list)) < 10:
+        length = int(len(file_list))
+
+    for i in range(length):
+        current_max = 0
+        for current in file_list:
+            if current in result_list:
+                continue
+            if current > current_max:
+                current_max = current
+        if current_max == 0:
+            break
+        else:
+            result_list.append(current_max)
+    return result_list
+
+
 if __name__ == '__main__':
-    Q1 = print_hi()
     result = {
-        'Q1': Q1
+        'Q1': print_hi(),
+        'Q2': get_sum(),
+        'Q3': get_max()
     }
-    # resultStr = json.dumps(result, sort_keys=True, indent=4, separators=(',', ':'))
-    # print(resultStr)
 
     # 打开一个文件
     with open("result.json", "w") as f:
         json.dump(result, f)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
